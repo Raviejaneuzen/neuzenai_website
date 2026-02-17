9cdaa365dc7bbe36bfd14372ed000bd538acb7bd
Raviejaneuzen <raviteja.u@neuzenai.com>
2026-02-17 17:30:55 +0530

    images updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -103,19 +103,6 @@ const Navbar = () => {
         >
           <div className="logo-wrapper">
             <img src="/logo.png" alt="NeuZenAI" className="navbar-logo-img" />
-            <div className="logo-divider"></div>
-            <div className="powered-by-wrapper">
-              <span className="powered-small">powered by</span>
-              <span className="z-ninth-big">
-                <span className="z-letter">z</span>
-                <span className="z-letter">-</span>
-                <span className="z-letter">n</span>
-                <span className="z-letter">i</span>
-                <span className="z-letter">n</span>
-                <span className="z-letter">t</span>
-                <span className="z-letter">h</span>
-              </span>
-            </div>
           </div>
         </Link>
 
@@ -210,6 +197,20 @@ const Navbar = () => {
             </svg>
           )}
         </button>
+      </div>
+
+      {/* Floating Brand Badge */}
+      <div className="navbar-floating-badge">
+        <span className="powered-small">powered by</span>
+        <span className="z-ninth-big">
+          <span className="z-letter">z</span>
+          <span className="z-letter">-</span>
+          <span className="z-letter">n</span>
+          <span className="z-letter">i</span>
+          <span className="z-letter">n</span>
+          <span className="z-letter">t</span>
+          <span className="z-letter">h</span>
+        </span>
       </div>
 
       {/* Mobile Menu */}
